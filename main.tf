77c612a8bdc8f1bc53b00829094404ca6678a7e2
Richard <richwats@cisco.com>
2021-11-22 18:56:11 +1100

    Updated test settings

--- main.tf
+++ main.tf
@@ -35,20 +35,22 @@ resource "thousandeyes_http_server" "http_tests" {
 
 
   // NOTE: Bug with API/Provider?  Need to save test in GUI to match settings
-  network_measurements = 0
-  mtu_measurements = 0
-  bandwidth_measurements = 0
-  bgp_measurements = 0
-  use_public_bgp = 0
-  // num_path_traces = 0
-
-  // // NOTE: Bug with API/Provider?  Need to save test in GUI to match settings
-  // network_measurements = 1
-  // mtu_measurements = 1
+  // Initial Setup
+  // network_measurements = 0
+  // mtu_measurements = 0
   // bandwidth_measurements = 0
-  // bgp_measurements = 1
-  // use_public_bgp = 1
+  // bgp_measurements = 0
+  // use_public_bgp = 0
   // // num_path_traces = 0
+
+
+  // Updated Settings
+  network_measurements = 1
+  mtu_measurements = 1
+  bandwidth_measurements = 0
+  bgp_measurements = 1
+  use_public_bgp = 1
+  // num_path_traces = 0
 
   dynamic "agents" {
     for_each = toset(var.agent_list)
