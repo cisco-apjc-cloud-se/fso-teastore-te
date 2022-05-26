597676870d907f1ea69f3cb97609506d1a27e19a
Richard <richwats@cisco.com>
2022-05-26 15:26:32 +1000

    back

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
   required_providers {
     thousandeyes = {
-    #   source = "william20111/thousandeyes"
+      # source = "william20111/thousandeyes"
       source = "cgascoig/cgascoig/thousandeyes"   # this is a custom build of the william20111/thousandeyes provider with a bug fixed (see https://github.com/william20111/terraform-provider-thousandeyes/issues/59)
       version = "0.6.0"
     }
@@ -34,22 +34,22 @@ resource "thousandeyes_http_server" "http_tests" {
   content_regex = ".*"
 
 
-  // NOTE: Bug with API/Provider?  Need to save test in GUI to match settings
-  // Initial Setup
-  // network_measurements = 0
-  // mtu_measurements = 0
-  // bandwidth_measurements = 0
-  // bgp_measurements = 0
-  // use_public_bgp = 0
-  // // num_path_traces = 0
-
-  // Updated Settings
-  network_measurements = 1
-  mtu_measurements = 1
+  # NOTE: Bug with API/Provider?  Need to save test in GUI to match settings
+  # Initial Setup
+  network_measurements = 0
+  mtu_measurements = 0
   bandwidth_measurements = 0
-  bgp_measurements = 1
-  use_public_bgp = 1
-  // num_path_traces = 0
+  bgp_measurements = 0
+  use_public_bgp = 0
+  # num_path_traces = 0
+
+  # # Updated Settings
+  # network_measurements = 1
+  # mtu_measurements = 1
+  # bandwidth_measurements = 0
+  # bgp_measurements = 1
+  # use_public_bgp = 1
+  # # num_path_traces = 0
 
   dynamic "agents" {
     for_each = toset(var.agent_list)
