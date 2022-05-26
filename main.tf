d2f2447ec8448a24b402e2b43dcffe424278f570
Richard <richwats@cisco.com>
2022-05-26 15:23:46 +1000

    retry

--- main.tf
+++ main.tf
@@ -34,22 +34,22 @@ resource "thousandeyes_http_server" "http_tests" {
   content_regex = ".*"
 
 
-  // NOTE: Bug with API/Provider?  Need to save test in GUI to match settings
-  // Initial Setup
-  // network_measurements = 0
-  // mtu_measurements = 0
-  // bandwidth_measurements = 0
-  // bgp_measurements = 0
-  // use_public_bgp = 0
-  // // num_path_traces = 0
-
-  // Updated Settings
-  network_measurements = 1
-  mtu_measurements = 1
+  # NOTE: Bug with API/Provider?  Need to save test in GUI to match settings
+  # Initial Setup
+  network_measurements = 0
+  mtu_measurements = 0
   bandwidth_measurements = 0
-  bgp_measurements = 1
-  use_public_bgp = 1
-  // num_path_traces = 0
+  bgp_measurements = 0
+  use_public_bgp = 0
+  # num_path_traces = 0
+
+  # # Updated Settings
+  # network_measurements = 1
+  # mtu_measurements = 1
+  # bandwidth_measurements = 0
+  # bgp_measurements = 1
+  # use_public_bgp = 1
+  # # num_path_traces = 0
 
   dynamic "agents" {
     for_each = toset(var.agent_list)
